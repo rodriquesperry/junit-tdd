60c9ce7fce2edeb661ec9240007d53aa4aecded8
Rodriques <rodriques.perry@gmail.com>
2021-11-15 10:18:07 -0600

    assertTrue and assertFalse

--- src/test/java/TestingWithJunit.java
+++ src/test/java/TestingWithJunit.java
@@ -49,4 +49,16 @@ public class TestingWithJunit {
 
         assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void testIfGreaterOrLesserThanWorks() {
+
+        boolean learningTDD = true;
+
+        assertTrue(learningTDD);
+        assertTrue("5 is greater than 4", 5 > 4);
+        assertFalse("5 is not greater than 6", 5 > 6);
+    }
+
+
 }
