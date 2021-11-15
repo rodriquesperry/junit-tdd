31c11a822d22f8ea826d55eac063529af9d73b86
Rodriques <rodriques.perry@gmail.com>
2021-11-15 09:56:28 -0600

    assertEquals assertion

--- src/test/java/TestingWithJunit.java
+++ src/test/java/TestingWithJunit.java
@@ -1,3 +1,31 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestingWithJunit {
 
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Hello World!");
+    }
+
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+
+    // When you're using floating points values a third parameter is required (Delta) which represents a positive margin of error of decimals.
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
 }
